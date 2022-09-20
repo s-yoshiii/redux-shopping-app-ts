16ed83d41a1d1632323e0dd91a7142a7b2713bcf
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-09-20 09:48:48 +0900

    ModalSliceを作成しよう

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 import cartReducer from "./features/cart/CartSlice";
+import modalReducer from "./features/modal/ModalSlice";
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
